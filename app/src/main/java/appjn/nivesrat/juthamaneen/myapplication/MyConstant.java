9c967d111b01e42887d21090f912aa55653c3e73
Puka_JN <nivesrat@hotmail.com>
2017-05-17 15:27:37 +0700

    upload class MyConstant Success 17 May 17

---
+++ app/src/main/java/appjn/nivesrat/juthamaneen/myapplication/MyConstant.java
@@ -0,0 +1,20 @@
+package appjn.nivesrat.juthamaneen.myapplication;
+
+/**
+ * Created by juthamanee.n on 17/05/2017.
+ */
+
+public class MyConstant {
+
+    //Explicit
+    private String urlPostUser = "http://androidthai.in.th/fetl/addUserJuthamanee.php";
+    private String urlGetUser = "http://androidthai.in.th/fetl/getUserJuthamanee.php";
+
+
+    public String getUrlPostUser() {
+        return urlPostUser;
+    }
+    public String getUrlGetUser() {
+        return urlGetUser;
+    }
+}//Main Class
