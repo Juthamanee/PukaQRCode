6018f2969751c8ff5e667660639661e2e09dc3f6
Puka_JN <nivesrat@hotmail.com>
2017-05-18 09:45:29 +0700

    show text success 18 May 17

--- app/src/main/java/appjn/nivesrat/juthamaneen/myapplication/ServiceActivity.java
+++ app/src/main/java/appjn/nivesrat/juthamaneen/myapplication/ServiceActivity.java
@@ -1,13 +1,38 @@
 package appjn.nivesrat.juthamaneen.myapplication;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ServiceActivity extends AppCompatActivity {
+    //Explicit
+    private TextView textView;
+    private ImageView barImageView, qrImageView;
+    private ListView listView;
+    private String nameString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+        //Initial View
+        initialView();
+
+        //Show text
+        nameString = getIntent().getStringExtra("Login");
+        textView.setText(nameString);
+
+    }//Main Method
+
+    private void initialView() {
+        textView = (TextView) findViewById(R.id.txtNameLogin);
+        barImageView = (ImageView) findViewById(R.id.imvBarCode);
+        qrImageView = (ImageView) findViewById(R.id.imvQrCode);
+        listView = (ListView) findViewById(R.id.LivProduct);
     }
-}
+}//Main Class
